f65f4926930d7fb176ebaf3323ffdea3ee0aba28
XieChencai <xiechencai@menusifu.cn>
2023-04-26 14:18:34 +0800

    update user info

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -44,12 +44,12 @@ onMounted(() => {
       <h2 class="text-xl font-bold">
         Version - {{ pkg.version }}
       </h2>
-      <div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">
+<!--      <div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">
         <p>
           此项目开源于
           <a
             class="text-blue-600 dark:text-blue-500"
-            href="https://github.com/Chanzhaoyu/chatgpt-web"
+            href="https://github.com/ChaceTse/chatgpt-web"
             target="_blank"
           >
             GitHub
@@ -59,7 +59,7 @@ onMounted(() => {
         <p>
           如果你觉得此项目对你有帮助，请在 GitHub 帮我点个 Star 或者给予一点赞助，谢谢！
         </p>
-      </div>
+      </div> -->
       <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
       <p v-if="isChatGPTAPI">
         {{ $t("setting.monthlyUsage") }}：{{ config?.usage ?? '-' }}
--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -3,9 +3,9 @@ import { ss } from '@/utils/storage'
 const LOCAL_NAME = 'userStorage'
 
 export interface UserInfo {
-  avatar: string
+  avatar?: string
   name: string
-  description: string
+  description?: string
 }
 
 export interface UserState {
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://avatars.githubusercontent.com/u/26705066',
+      // avatar: '', // 'https://avatars.githubusercontent.com/u/26705066',
       name: 'Easy AI',
-      description: 'Star on <a href="https://github.com/ChaceTse/chatgpt-web" class="text-blue-500" target="_blank" >GitHub</a>',
+      // description: '', // 'Star on <a href="https://github.com/ChaceTse/chatgpt-web" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
